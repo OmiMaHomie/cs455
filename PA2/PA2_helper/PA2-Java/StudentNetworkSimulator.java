1f17ee4f41bad921ec0809fb1abd8b62db9278af
Om Khadka <om.khadka.101@gmail.com>
2026-04-12 01:16:14 -0400

    SR functional

--- PA2/PA2_helper/PA2-Java/StudentNetworkSimulator.java
+++ PA2/PA2_helper/PA2-Java/StudentNetworkSimulator.java
@@ -3,110 +3,25 @@ import java.io.*;
 
 public class StudentNetworkSimulator extends NetworkSimulator
 {
-    /*
-     * Predefined Constants (static member variables):
-     *
-     *   int MAXDATASIZE : the maximum size of the Message data and
-     *                     Packet payload
-     *
-     *   int A           : a predefined integer that represents entity A
-     *   int B           : a predefined integer that represents entity B 
-     *
-     * Predefined Member Methods:
-     *
-     *  void stopTimer(int entity): 
-     *       Stops the timer running at "entity" [A or B]
-     *  void startTimer(int entity, double increment): 
-     *       Starts a timer running at "entity" [A or B], which will expire in
-     *       "increment" time units, causing the interrupt handler to be
-     *       called.  You should only call this with A.
-     *  void toLayer3(int callingEntity, Packet p)
-     *       Puts the packet "p" into the network from "callingEntity" [A or B]
-     *  void toLayer5(String dataSent)
-     *       Passes "dataSent" up to layer 5
-     *  double getTime()
-     *       Returns the current time in the simulator.  Might be useful for
-     *       debugging.
-     *  int getTraceLevel()
-     *       Returns TraceLevel
-     *  void printEventList()
-     *       Prints the current event list to stdout.  Might be useful for
-     *       debugging, but probably not.
-     *
-     *
-     *  Predefined Classes:
-     *
-     *  Message: Used to encapsulate a message coming from layer 5
-     *    Constructor:
-     *      Message(String inputData): 
-     *          creates a new Message containing "inputData"
-     *    Methods:
-     *      boolean setData(String inputData):
-     *          sets an existing Message's data to "inputData"
-     *          returns true on success, false otherwise
-     *      String getData():
-     *          returns the data contained in the message
-     *  Packet: Used to encapsulate a packet
-     *    Constructors:
-     *      Packet (Packet p):
-     *          creates a new Packet that is a copy of "p"
-     *      Packet (int seq, int ack, int check, String newPayload)
-     *          creates a new Packet with a sequence field of "seq", an
-     *          ack field of "ack", a checksum field of "check", and a
-     *          payload of "newPayload"
-     *      Packet (int seq, int ack, int check)
-     *          chreate a new Packet with a sequence field of "seq", an
-     *          ack field of "ack", a checksum field of "check", and
-     *          an empty payload
-     *    Methods:
-     *      boolean setSeqnum(int n)
-     *          sets the Packet's sequence field to "n"
-     *          returns true on success, false otherwise
-     *      boolean setAcknum(int n)
-     *          sets the Packet's ack field to "n"
-     *          returns true on success, false otherwise
-     *      boolean setChecksum(int n)
-     *          sets the Packet's checksum to "n"
-     *          returns true on success, false otherwise
-     *      boolean setPayload(String newPayload)
-     *          sets the Packet's payload to "newPayload"
-     *          returns true on success, false otherwise
-     *      int getSeqnum()
-     *          returns the contents of the Packet's sequence field
-     *      int getAcknum()
-     *          returns the contents of the Packet's ack field
-     *      int getChecksum()
-     *          returns the checksum of the Packet
-     *      int getPayload()
-     *          returns the Packet's payload
-     *
-     */
+    //
+    // FIELDS
+    //
 
-    /*   Please use the following variables in your routines.
-     *   int WindowSize  : the window size
-     *   double RxmtInterval   : the retransmission timeout
-     *   int LimitSeqNo  : when sequence number reaches this value, it wraps around
-     */
-
+    // DONT TOUCH
     public static final int FirstSeqNo = 0;
     private int WindowSize;
     private double RxmtInterval;
-    private int LimitSeqNo;
-    
-    // Add any necessary class variables here.  Remember, you cannot use
-    // these variables to send messages error free!  They can only hold
-    // state information for A or B.
-    // Also add any necessary methods (e.g. checksum of a String)
+    private int LimitSeqNo; 
 
     // Entity A
-    private int base;                  // Oldest unACKed seq. # (Logical)
-    private int nextseqnum;            // Next seq # to send (Logical)
-    private Packet[] snd_buf;          // window buffer
-    private ArrayList<Message> pendingMessages; // Queue, for msgs waiting for window
+    private int base;   // Oldest unACKed sequence number (Logical)
+    private int nextseqnum; // Next sequence number to send (Logical)
+    private Packet[] snd_buf; // Sender window buffer
+    private ArrayList<Message> pendingMessages; // Queue for messages waiting for window
     
     // Entity B
-    private int expectedseqnum;        // Next packet expected, in order (Logical)
-    private Packet[] rcv_buf;          // Receiver buffer, for out-of-order packets
+    private int expectedseqnum; // Next packet expected in order (Logical)
+    private Packet[] rcv_buf; // Receiver buffer, for out-of-order packets
     
     // Debugging
     private int numOriginalPackets;
@@ -116,26 +31,9 @@ public class StudentNetworkSimulator extends NetworkSimulator
     private int numCorruptedPackets;
     private int totalPacketsSent;
 
-    // Calc. Checksum
-    private int calculateChecksum(Packet packet) {
-        int checksum = 0;
-        checksum += packet.getSeqnum();
-        checksum += packet.getAcknum();
-        String payload = packet.getPayload();
-
-        if (payload != null) {
-            for (int i = 0; i < payload.length(); i++) {
-                checksum += (int) payload.charAt(i);
-            }
-        }
-
-        return checksum;
-    }
-    
-    // Is packet corrupted (Y/N)?
-    private boolean isCorrupted(Packet packet) {
-        return calculateChecksum(packet) != packet.getChecksum();
-    }
+    //
+    // CONSTRUCTOR(S)
+    //
 
     // This is the constructor.  Don't touch!
     public StudentNetworkSimulator(int numMessages,
@@ -149,7 +47,7 @@ public class StudentNetworkSimulator extends NetworkSimulator
     {
         super(numMessages, loss, corrupt, avgDelay, trace, seed);
         WindowSize = winsize;
-        LimitSeqNo = winsize*2; // set appropriately; assumes SR here!
+        LimitSeqNo = winsize*2; // Set to SR
         RxmtInterval = delay;
 
         // Custom init. (for the debugging)
@@ -161,43 +59,64 @@ public class StudentNetworkSimulator extends NetworkSimulator
         totalPacketsSent = 0;
     }
 
+    //
+    // METHODS
+    //
+
+    // Checksum calc.
+    private int calculateChecksum(Packet packet) {
+        int checksum = 0;
+        checksum += packet.getSeqnum();
+        checksum += packet.getAcknum();
+        String payload = packet.getPayload();
+
+        // This packet got a payload?
+        // YES
+        if (payload != null) {
+            for (int i = 0; i < payload.length(); i++) {
+                checksum += (int) payload.charAt(i);
+            }
+        }
+
+        // NO --> do nothing
+
+        return checksum;
+    }
     
-    // This routine will be called whenever the upper layer at the sender [A]
-    // has a message to send.  It is the job of your protocol to insure that
-    // the data in such a message is delivered in-order, and correctly, to
-    // the receiving upper layer.
+    // Corruption check (just checks sent checksum to packet checksum)
+    private boolean isCorrupted(Packet packet) {
+        return calculateChecksum(packet) != packet.getChecksum();
+    }
+
+
+    // A wants to send a msg (layer 5)
     protected void aOutput(Message message)
     {
-        /* 
-        Add the msg the queue, and then try to send as much as possible.
-        NOTE: Look at sendPackets() for more info.
-        */
+        // Add msg to queue, and just send AMAP (as much as possible)
         pendingMessages.add(message);
         sendPackets();
     }
 
-    // Packet-sending logic, for aOutput(Message msg)
+    // Helper for aOutput: Handles logic when the window isn't full (& there's pending msgs)
     private void sendPackets() {
-        /* WHILE:
-            - Still got [pending] msgs &&
-            - Window isn't full
-
-        DO:
-            - Make a packet
-            - Store it in window buffer
-            - Send to layer3
-            - Upd. stat
-            - Start timer (if it's the oldest packet)
-            - Move onto th next seq. #
-        */
         while (!pendingMessages.isEmpty() && (nextseqnum < base + WindowSize)) {
             Message msg = pendingMessages.remove(0);
             
-            int seq = nextseqnum % LimitSeqNo; // seq # = nextSeq# MOD limitSeq#
+            // Packet has seq# = MOD limitSeq#
+            int seq = nextseqnum % LimitSeqNo;
             Packet pkt = new Packet(seq, -1, 0, msg.getData());
             pkt.setChecksum(calculateChecksum(pkt));
             
-            snd_buf[nextseqnum % WindowSize] = pkt; // MOD WindowSize is index
+            /* Then do the following:
+                - Store in snd win buf (MOD WindowSize as index)
+                - Send to layer3
+                - Debug update
+                - Start the timer:
+                    - ONLY if the current packet is the FIRST packet being sent (as in, it's base).
+                    - If we're sending a prev. packet, its timer would alr be running.
+                - Update next seq# num
+            */
+            snd_buf[nextseqnum % WindowSize] = pkt;
             
             toLayer3(A, pkt);
             
@@ -212,13 +131,9 @@ public class StudentNetworkSimulator extends NetworkSimulator
         }
     }
     
-    // This routine will be called whenever a packet sent from the B-side 
-    // (i.e. as a result of a toLayer3() being done by a B-side procedure)
-    // arrives at the A-side.  "packet" is the (possibly corrupted) packet
-    // sent from the B-side.
+    // A gets a packet (its an ACK, based on assignment details)
     protected void aInput(Packet packet)
     {
-        // Packet corruption check
         if (isCorrupted(packet)) {
             numCorruptedPackets++;
             return;
@@ -226,62 +141,72 @@ public class StudentNetworkSimulator extends NetworkSimulator
 
         int ack = packet.getAcknum();
 
-        /* Now, process the Cumulative ACK:
-            - Map the ACK to the logical seq. # RELATIVE to base.
-            - If the diff < WindowSize, then the ACK is valid (current window range).
-                - Because this is SR, a new ACK will just ACK everything up to localACKed.
-                - And then get more pending msgs, now that window moved.
-            - Else, it's invalid (and prolly a dup).
-                - Check if the dup'ed ACK is from base - 1, because if so, we can just retransmit that packet.
+        /* Since this is SR, process ACK as a Cumulative ACK:
+            - Map the ACK (mod value) to the logicalseq# RELATIVE to base
+            - Check if ACK is within current window range
+                - If so, it's a new ACK --> ACK everything up to logicalACKed
+                - Else, it's prolly a dup --> resend missing packet (check if the dup is actually for base - 1 tho [missing base])
         */
-        int baseMod = base % LimitSeqNo; // MOD value
+        int baseMod = base % LimitSeqNo;
         int diff = (ack - baseMod + LimitSeqNo) % LimitSeqNo;
         
         if (diff < WindowSize) {
             int logicalAcked = base + diff;
             
             if (logicalAcked >= base && logicalAcked < nextseqnum) {
-                base = logicalAcked + 1;
+                base = logicalAcked + 1;    // Move base forward
                 
+                // Is Window empty?
+                // YES --> stop timer
                 if (base == nextseqnum) {
-                    stopTimer(A); // All packets ACKed
+                    stopTimer(A);
                 } 
+                // NO --> Stop old timer, then start new one
                 else {
-                    startTimer(A, RxmtInterval); // Restart timer for new base
+                    stopTimer(A);
+                    startTimer(A, RxmtInterval);
                 }
                 
-                sendPackets();
+                sendPackets();  // Try to send more pending messages now that window has moved
             }
         } 
         else {
             int expectedDup = (base - 1 + LimitSeqNo) % LimitSeqNo;
-            if (ack == expectedDup && base > 0) {
-                aTimerInterrupt();
+            if (ack == expectedDup) {
+                // I dunno if I can just call this. Just gonna copy paste the code to hard-wire it.
+                // aTimerInterrupt();
+                Packet p = snd_buf[base % WindowSize];
+
+                if (p != null) {
+                    toLayer3(A, p);
+                    numRetransmissions++;
+                    totalPacketsSent++;
+
+                    // Restart timer
+                    stopTimer(A);
+                    startTimer(A, RxmtInterval);
+                }
             }
         }
     }
     
-    // This routine will be called when A's timer expires (thus generating a 
-    // timer interrupt). You'll probably want to use this routine to control 
-    // the retransmission of packets. See startTimer() and stopTimer(), above,
-    // for how the timer is started and stopped. 
+    // A's packet expired
+    // Just rtransmit the oldest unACKed packet [base]
     protected void aTimerInterrupt()
     {
-        // Retransmit oldest unacked packet (base)
         Packet p = snd_buf[base % WindowSize];
 
         if (p != null) {
             toLayer3(A, p);
             numRetransmissions++;
             totalPacketsSent++;
+
+            // Restart timer
             startTimer(A, RxmtInterval);
         }
     }
     
-    // This routine will be called once, before any of your other A-side 
-    // routines are called. It can be used to do any required
-    // initialization (e.g. of member variables you add to control the state
-    // of entity A).
+    // A (sender) init
     protected void aInit()
     {
         base = 0;
@@ -290,10 +215,7 @@ public class StudentNetworkSimulator extends NetworkSimulator
         pendingMessages = new ArrayList<Message>();
     }
     
-    // This routine will be called whenever a packet sent from the B-side 
-    // (i.e. as a result of a toLayer3() being done by an A-side procedure)
-    // arrives at the B-side.  "packet" is the (possibly corrupted) packet
-    // sent from the A-side.
+    // B gets a packet
     protected void bInput(Packet packet)
     {
         // Packet corruption check
@@ -352,7 +274,7 @@ public class StudentNetworkSimulator extends NetworkSimulator
         }
     }
 
-    // Sends ACK
+    // Helper method for bInput(): Sends ACK
     private void sendACK(int ackNum) {
         /* Ok so this method does the following:
             - since ACK# is logical, set the newACK# %= limitSeq#
@@ -368,18 +290,14 @@ public class StudentNetworkSimulator extends NetworkSimulator
         numACKsSent++;
     }
 
-    
-    // This routine will be called once, before any of your other B-side 
-    // routines are called. It can be used to do any required
-    // initialization (e.g. of member variables you add to control the state
-    // of entity B).
+    // B (reciever) init
     protected void bInit()
     {
         expectedseqnum = 0;
         rcv_buf = new Packet[WindowSize];
     }
 
-    // Use to print final statistics
+    // Debug/Stats
     protected void Simulation_done()
     {
     	// TO PRINT THE STATISTICS, FILL IN THE DETAILS BY PUTTING VARIBALE NAMES. DO NOT CHANGE THE FORMAT OF PRINTED OUTPUT
